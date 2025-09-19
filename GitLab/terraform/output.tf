f887b29a5575a80583c5be90b77d5c56def66602
Shruti Bangre <shrutibokade@gmail.com>
2025-09-19 17:32:32 +0530

    Fix output.tf to reference Docker_Server correctly

--- GitLab/terraform/output.tf
+++ GitLab/terraform/output.tf
@@ -1,7 +1,29 @@
+# Output the public IP of the Docker server
 output "instance_public_ip" {
-  value = aws_instance.web.public_ip
+  description = "Public IP address of the Docker EC2 instance"
+  value       = aws_instance.Docker_Server.public_ip
 }
 
+# Output the public DNS of the Docker server
 output "instance_public_dns" {
-  value = aws_instance.web.public_dns
+  description = "Public DNS of the Docker EC2 instance"
+  value       = aws_instance.Docker_Server.public_dns
+}
+
+# Output the VPC ID
+output "vpc_id" {
+  description = "ID of the created VPC"
+  value       = aws_vpc.main.id
+}
+
+# Output the Subnet ID
+output "subnet_id" {
+  description = "ID of the public subnet"
+  value       = aws_subnet.public.id
+}
+
+# Output the Security Group ID
+output "security_group_id" {
+  description = "ID of the security group"
+  value       = aws_security_group.web_sg.id
 }
